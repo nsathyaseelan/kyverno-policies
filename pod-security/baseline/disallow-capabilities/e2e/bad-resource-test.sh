206465de9c26f6067a2f04b315d4a0721024b0b9
nsathyaseelan <sathyaseelan@nirmata.com>
2023-12-15 17:27:48 +0530

    Added the Chainsaw test
    Signed-off-by: nsathyaseelan <sathyaseelan@nirmata.com>

--- pod-security/baseline/disallow-capabilities/e2e/bad-resource-test.sh
+++
@@ -1,8 +0,0 @@
-if  kubectl apply -f bad-resource.yaml 2>&1 | grep -q  "created" 
-then 
-  echo "Test failed, Resource creation is not blocked when the policy is in Enforce mode."
-  exit 1
-else 
-  echo "Test succeed, Resource creation is blocked when the policy is in Enforce mode."
-  exit 0
-fi
